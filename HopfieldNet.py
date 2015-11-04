401d14514fc99ca4e00666e2170068d0a962dbc5
jseptienr <septien.jorge@gmail.com>
2015-11-03 17:57:17 -0800

    First commit

---
+++ HopfieldNet.py
@@ -0,0 +1,26 @@
+class HopfieldNet:
+	def __init__(self, size):
+		self.size = size
+		self.neurons = []
+		self.weights = []
+
+	def initialize():
+		""" Initialize Network """
+		pass
+	
+	def train(self, input_vector):
+		""" Train Network """
+		pass
+	
+	def test(self, input_vector):
+		"""Test Network """
+		x_input = input_vector[:]
+		y_output = []
+		for x in x_input:
+			 
+		pass
+
+
+if __name__ == '__main__':
+	print('Hopfield Network')	
+	
